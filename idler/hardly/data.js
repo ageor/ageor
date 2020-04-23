4a1a438b938abbcab45462de3bd2992aebe569d8
Anton Georgiev <anton.v.georgiev@gmail.com>
2020-04-23 20:25:37 +0300

    Add entire second Biz (copy of first)

--- idler/hardly/data.js
+++ idler/hardly/data.js
@@ -2,7 +2,7 @@ const Components = ["biome.js","biz.js","generator.js","text_set.js","ui/base_do
 
 const Systems = ["base_ui_system.js","biome_system.js","biome_ui_system.js","biz_system.js","biz_ui_system.js","event_system.js","generator_system.js","generator_ui_system.js","localization_system.js","time_system.js"];
 
-const Prototypes = ["biomes/west_town.json","biz/barn.json","generator/chicken.json","generator/cow.json","generator/donkey.json","generator/goat.json","generator/horse.json","generator/sheep.json","locale/en.json"];
+const Prototypes = ["biomes/west_town.json","biz/barn.json","biz/farm.json","generator/apples.json","generator/beets.json","generator/carrots.json","generator/chicken.json","generator/corn.json","generator/cow.json","generator/donkey.json","generator/goat.json","generator/horse.json","generator/pears.json","generator/sheep.json","generator/wheat.json","locale/en.json"];
 
 const Assets = [];
 
